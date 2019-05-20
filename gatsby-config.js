4a3f182595b7ecb19a9c36feb0f58473e2645f6c
Eric Kramer <theerickramer@gmail.com>
2019-05-20 11:47:37 -0400

    removes defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Eric Kramer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `The official website of Eric Kramer`,
+    author: `Eric Kramer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
