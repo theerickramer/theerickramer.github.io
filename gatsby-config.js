0b2bb7d24e9c76788510b8ef781d7ba079605dab
Eric Kramer <theerickramer@gmail.com>
2019-05-21 15:12:23 -0400

    adds google font prefetch

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,20 @@ module.exports = {
         path: `./src/data/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`thin`],
+          },
+          {
+            family: `Karla`,
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
